010a7d59702e8c9b01c82b07a6a04bcea0453704
xubin <xpin_1509@163.com>
2020-11-05 20:30:53 +0800

    fix

--- plugin/bable-plugin-arrow-function.js
+++ plugin/bable-plugin-arrow-function.js
@@ -3,18 +3,18 @@ module.exports = function({ types: t }) {
       visitor: {
         //代表处理 ArrowFunctionExpression 节点
         ArrowFunctionExpression(path, state){
-            // 原来的参数
-            const  params = path.node.params
-            // 创建一个blockStatement
+          // 原来的参数
+          const  params = path.node.params
+          // 创建一个blockStatement
 
-            const blockStatement = t.blockStatement([
-                t.returnStatement(path.node.body)
-            ])
+          const blockStatement = t.blockStatement([
+              t.returnStatement(path.node.body)
+          ])
 
-            // 创建一个函数
-            const fn = t.functionExpression(null, params, blockStatement, false, false)
+          // 创建一个函数
+          const fn = t.functionExpression(null, params, blockStatement, false, false)
 
-            path.replaceWith(fn)
+          path.replaceWith(fn)
         }
       }
     };
